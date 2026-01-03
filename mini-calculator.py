b11f0fba3d0c8fc6d50dce0ba4b96b128209ccbe
len0-0 <246881668+len-531@users.noreply.github.com>
2026-01-03 16:17:31 +0400

    small updates
    less repetition
    simpler
    while loop....

--- mini-calculator.py
+++ mini-calculator.py
@@ -1,22 +1,27 @@
 print("----------MINI-CALCULATOR----------")
-print("Menu")
-print("1.ADDITION")
-print("2.SUBTRACTION")
-print("3.MULTIPLICATION")
-print("4.DIVISION")
-a=int(input("Enter first input: "))
-b=int(input("Enter second input: "))
-op=int(input("Enter ur option: "))
-if op==1:
-    print("SUM OF",a,"and",b,"IS",a+b)
-elif op==2:
-    print("DIFFERENCE OF",a,"and",b,"IS",a-b)
-elif op==3:
-    print("PRODUCT OF",a,"and",b,"IS",a*b)
-elif op==4:
-    if b==0:
-        print("NOT DEFINED")
+while True:
+    print("\nMenu")
+    print("1.ADDITION")
+    print("2.SUBTRACTION")
+    print("3.MULTIPLICATION")
+    print("4.DIVISION")
+    op=int(input("Enter ur option: "))
+    if op in [1, 2, 3, 4]:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+    if op==1:
+        print("SUM: ",a+b)
+    elif op==2:
+        print("DIFFERENCE:",a-b)
+    elif op==3:
+        print("PRODUCT:",a*b)
+    elif op==4:
+        if b==0:
+            print("NOT DEFINED")
+        else:
+            print("QUOTIENT: ",a/b)
     else:
-        print("QUOTIENT OF",a,"and",b,"IS",a/b)
-else:
-    print("INVALID OPTION")
+        print("INVALID OPTION")
+    ch=input("Do u want to continue y/n?:")
+    if ch.lower() =='n':
+        break
